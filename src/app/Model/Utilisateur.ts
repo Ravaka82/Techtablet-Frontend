7fbac295aac67ccaac46d31053a692d9af432082
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2023-02-22 23:24:10 +0300

    update registration with first name

--- src/app/Model/Utilisateur.ts
+++ src/app/Model/Utilisateur.ts
@@ -2,7 +2,10 @@ import { Role } from "./Role";
 export class Utilisateur{
     id!: string;
     nom!: string;
+    prenom!: string;
     email!: string;
     mot_de_passe!: string;
+    totalpayer!: string;
+    lieu_livraison!: string;
     roles!: Role;
 }
